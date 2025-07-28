99be51720dd38f1dbba3b05ac82f2e2dfddf20ef
sunsesa <sunsea_h@163.com>
2025-07-28 14:23:29 +0800

    更新主题

--- scripts/fluid-theme-inject.js
+++ scripts/fluid-theme-inject.js
@@ -1,3 +1,5 @@
 hexo.extend.filter.register('theme_inject', function(injects) {
+  if (hexo.config.theme !== 'fluid') return;
+
   injects.head.file('content_blocks', 'source/_inject/head.ejs');
 });
